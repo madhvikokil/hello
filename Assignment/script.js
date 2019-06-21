c7317190a7b1db3b2ddcc2dce85177b8e6766b4e
Madhvi Kokil <madhvi.kokil@cuelogic.com>
2019-06-21 20:12:08 +0530

    21 June changes

--- Assignment/script.js
+++ Assignment/script.js
@@ -1,15 +1,78 @@
-function allLetter(text1,password){ 
-      var letters = /^[A-Za-z]+$/;
-      if(text1.value.match(letters))
-      {
-      alert('Your name have accepted : you can try another');
-      return true;
-      }
-      else
-      {
-      alert('Please input alphabet characters only');
-      return false;
-      }
+// Validation of Username and password
 
-      
+function validation(){
+  var uname = document.login.uname.value;
+  var pwd = document.login.pwd.value;
+
+  if(uname=="")
+  {
+      alert("Username should not be blank");
+      return;
+  }
+  else if(pwd =="")
+  { 
+    alert("Password should not be blank");
+    return;
+  }
+  var usenameRegex = "(*[a-z])(*[A-Z]).{4,}";
+  var passwordRegex = "(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}";
+  /* var passwordResult = pwd .match(passwordRegex); */
+  if((pwd.match(passwordRegex)) && (uname.match(usenameRegex)))
+  {
+
+      document.write(window.open('https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match'));
+     
+  }
+  else{ 
+      alert('Wrong...!')
+  }
+  /* var passwordRegex = "(?=.*[a-z])(?=.*[A-Z]).{2,}";
+  if)
+  {
+
+    document.write(window.open('https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match'));
+     
+  }
+  else{ 
+      alert('Wrong...!')
+  } */
+
 }
+
+// For sign-up page
+
+function register() {
+  var fname = document.login.fname.value;
+  var lname = document.login.lname.value;
+  var address = document.login.address.value;
+
+  if(fname=="" ||lname =="" ||address==""){
+    alert("Fields should not be blank");
+    return;
+}
+
+var firstnameRegex = '[a-zA-Z]';
+var firstnameResult = fname.match(firstnameRegex);
+
+if(!firstnameResult){
+  alert("invalid");
+}
+
+var lastnameRegex = '[a-zA-Z]';
+var lastnameResult = lname.match(lastnameRegex);
+
+if(!lastnameRegex){
+  alert("invalid");
+}
+
+var addressRegex = '[a-zA-Z]';
+var addressResult = address.match(addressRegex);
+
+
+document.write(window.open('https://www.the-art-of-web.com/javascript/validate-password/'));
+
+
+
+}
+
+
